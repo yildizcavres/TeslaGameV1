741f3a1a49559b239fafe71e750d2a22dc77c917
yildizcavres <129795323+yildizcavres@users.noreply.github.com>
2023-04-11 06:08:17 +0300

    Update botTesla.js

--- botTesla.js
+++ botTesla.js
@@ -7,9 +7,9 @@ require("dotenv").config();
 
 const { Telegraf, Telegram } = require("telegraf")
 
-const TOKEN = process.env.TOKEN || '';
+const TOKEN = process.env.TOKEN || '6283385917:AAE34O93Ab2FwsBfBu6qYmgcetIllmM9xpI';
 
-const ID_BOT = process.env.ID_BOT || '';
+const ID_BOT = process.env.ID_BOT || '6283385917';
 
 
 const config = require("./config")
